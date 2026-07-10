4b2fc26890ec71039ef7f36559a747f9a44c9036
eddobrev <eddobrev@amazon.com>
2026-07-10 08:45:00 -0500

    Bump version from 1.83 to 2.0

--- Hydra.meta.js
+++ Hydra.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hydra
 // @namespace    https://github.com/dylbecke/hydra-userscript
-// @version      1.83
+// @version      2.0
 // @description  Dual-headed load tracker - Inbound (red) and Outbound (blue) on Dock pages
 // @author       eddobrev
 // @updateURL    https://raw.githubusercontent.com/dylbecke/hydra-userscript/main/Hydra.meta.js
