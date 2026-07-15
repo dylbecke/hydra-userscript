6a1c2deb77bf5377622f4687d831aa0468b2a1bb
eddobrev <eddobrev@amazon.com>
2026-07-15 11:34:02 -0500

    Delete Hydra.meta.js

--- Hydra.meta.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name         Hydra
-// @namespace    https://github.com/dylbecke/hydra-userscript
-// @version      2.0
-// @description  Dual-headed load tracker - Inbound (red) and Outbound (blue) on Dock pages
-// @author       eddobrev
-// @updateURL    https://raw.githubusercontent.com/dylbecke/hydra-userscript/main/Hydra.meta.js
-// @downloadURL  https://raw.githubusercontent.com/dylbecke/hydra-userscript/main/Hydra.user.js
-// ==/UserScript==
